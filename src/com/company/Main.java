b206cfe4ca01740dce0602908f816f8d6c83c232
spetseffect <spetseffect@gmail.com>
2021-03-14 18:47:23 +0200

    case 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -25,38 +25,40 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Scanner scS=new Scanner(System.in);
-        String str=scS.nextLine();
-        scS.close();
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
         String[] s=str.split(",\s?");
         ArrayList<Integer> arr=new ArrayList<>();
         for (String i :s)
             arr.add(Integer.valueOf(i));
         ShowMenu();
-        Scanner scN=new Scanner(System.in);
-        Integer n=scN.nextInt();
-        switch (n){
-            case 1: {
-                System.out.print("Введите число: ");
-                Integer k=scN.nextInt();
-                arr.add(k);
-                break;
+        Integer n=-1;
+        while (!n.equals(0)) {
+            n = sc.nextInt();
+            switch (n) {
+                case 1 -> {
+                    System.out.print("Введите число: ");
+                    Integer k = sc.nextInt();
+                    arr.add(k);
+                }
+                case 2 -> {
+                    System.out.println("Введите порядковый номер элемента, который надо удалить: ");
+                    int i = sc.nextInt();
+                    System.out.println("Элемент с индексом " + (i - 1) +
+                            " и значением " + ((int) arr.remove(i - 1)) + " удалён.");
+                    ArrToConsole(arr);
+                }
+                case 3 -> {
+                    ArrToConsole(arr);
+                }
+                case 4 -> {
+                }
+                case 5 -> {
+                }
+                default -> System.out.println("Выход.");
             }
-            case 2: {
-                break;
-            }
-            case 3: {
-                ArrToConsole(arr);
-                break;
-            }
-            case 4: {
-                break;
-            }
-            case 5: {
-                break;
-            }
-            default: System.out.println("Выход.");
+            if(!n.equals(0)) ShowMenu();
         }
-        scN.close();
+        sc.close();
     }
 }
